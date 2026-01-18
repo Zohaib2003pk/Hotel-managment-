5e0a70b5ffa1989eda67de3c4579a4735008b31d
Zohaib <163235235+Zohaib2003pk@users.noreply.github.com>
2026-01-18 18:56:08 +0500

    Insertion of many more test classes

--- Test_HotelManagementSystem.java
+++ Test_HotelManagementSystem.java
@@ -38,3 +38,29 @@ public class HotelManagementSystemTest {
         assertNotNull(customer);
         assertNotNull(customer.getId());
     }
+		@Test
+    public void testMakeReservation() {
+        HotelManagementSystem.Hotel hotel =
+                new HotelManagementSystem.Hotel("Grand Palace");
+
+        hotel.addRoom(new HotelManagementSystem.Room(
+                101, HotelManagementSystem.RoomType.SINGLE, 100.0));
+
+        HotelManagementSystem.Customer customer =
+                new HotelManagementSystem.Customer(
+                        new HotelManagementSystem.Name("Ali", "Khan"),
+                        new HotelManagementSystem.Address("A", "B", "C"),
+                        new HotelManagementSystem.CreditCard("2222", "11/29")
+                );
+        Date today = new Date(0);
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_YEAR, 2);
+        java.util.Date later = cal.getTime();
+
+        HotelManagementSystem.Reservation reservation =
+                hotel.makeReservation(customer, today, later,
+                        HotelManagementSystem.RoomType.SINGLE);
+
+        assertNotNull(reservation);
+    }
+}
